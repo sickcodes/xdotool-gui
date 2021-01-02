edae5a618ffa1d113d4f987cc03d3cad92185d9c
sickcodes <info@sick.codes>
2021-01-02 03:08:33 +0000

    WIP Upgraded to GTK3, Python3 and GPLv3

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # xdotool-gui
-GUI for command-line X11 automation tool
+## [Follow @sickcodes on Twitter](https://twitter.com/sickcodes)
+
+GUI for command-line X11 automation tool.
+
+This project is maintained fork of the 2010 project by esbat.
+
+### V3.0
+# Features In xdotool-gui v3.0
+- Record xdotool sequences for X automation.
+- Upgraded to GTK3.
+- Upgraded to python 3.
+- Python requirements.txt
+
+# Credits
+https://twitter.com/sickcodes
+
+https://twitter.com/esbatmop
+
+![Xdotool GUI](/xdotool.gif?raw=true "Xdotool GUI")
+
+Authors: esbat==2010, Sick.Codes>=2021 
+
+https://sick.codes/ & https://twitter.com/sickcodes
+
+PR & Contributor Credits: https://github.com/sickcodes/xdotool-gui/blob/master/CREDITS.md
+
--- xdotoolgui.py
+++ xdotoolgui.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+# xdotool-gui v3.0
+# License: GPLv3
+# Notice Written by Sick.Codes, January 2021
+# Authors:  @sickcodes
+
+
+#!/usr/bin/env python
 #
 #       xdotoolgui.py - GUI for xdotool
 #       
@@ -19,43 +26,60 @@
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
 
+import sys
+import time
+# import thread
+import subprocess
+import string
 
-import sys,runxdotool,glib,time,thread
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+from gi.repository import Gdk
+from gi.repository import Gio
+from gi.repository import GLib
+
+
 try:
     import pygtk
     pygtk.require("2.0")
 except:
     pass
-try:
-    import gtk
-    import gtk.glade
-except:
-    sys.exit(1)
+# try:
+#     import gtk
+#     import gtk.glade
+# except:
+#     sys.exit(1)
 
-filename = 'new file'
+filename = '.xdotool-script'
 MouseLocation = False
 
 class xdotoolgui:
     """A gui for xdotool. Make it esay to used in gnome. """
 
     def __init__(self):
-        
         #Set the Glade file
-        self.wtree = gtk.glade.XML(sys.path[0]+'/xdotoolgui.glade')
-        self.wtree.get_widget('window1').set_title('xdotool-gui')
-        self.window = self.wtree.get_widget("window1")
+        self.builder = Gtk.Builder()
+        try:
+            self.builder.add_from_file("xdotoolgui.glade")
+        except:
+            print("file not found")
+            sys.exit()
+
+        self.builder.get_object('window1').set_title('xdotool-gui')
+        self.window = self.builder.get_object("window1")
         self.window.show()
-        #Create our dictionay and connect it
+        #Create our dictionary and connect it
         dic = { "on_imagemenuitem10_activate" : self.aboutdialog1_activate,
             "on_imagemenuitem1_activate" : self.newtext_activate,
             "on_imagemenuitem2_activate" : self.open_activate,
             "on_imagemenuitem3_activate" : self.save_activate,
             "on_imagemenuitem4_activate" : self.saveother_activate,
-            "on_imagemenuitem5_activate" : gtk.main_quit,  
+            # "on_imagemenuitem5_activate" : self.main_quit, ##########
             "on_button1_clicked" : self.move_mouse,
             "on_button3_clicked" : self.click_mouse,
             "on_button4_clicked" : self.delay_time,
-            "on_window1_destroy" : gtk.main_quit,
+            # "on_window1_destroy" : self.main_quit, ##################
             "on_cancel_clicked" : self.filesaveothercancel,
             "on_saveother_clicked": self.filesaveother,
             "on_movemousecancel_clicked" : self.movemousecancel,
@@ -72,115 +96,115 @@ class xdotoolgui:
             "on_togglebutton1_toggled" : self.mouselocation,
             "on_window1_expose_event" : self.threadmouse,
             "on_aboutdialog1_close" : self.quitabout}
-        self.wtree.signal_autoconnect(dic)
-        self.wtree.get_widget('statusbar1').push(self.wtree.get_widget('statusbar1').get_context_id("file name"),filename)
+        self.builder.connect_signals(dic)
+        self.builder.get_object('statusbar1').push(self.builder.get_object('statusbar1').get_context_id("file name"),filename)
         #thread.start_new_thread(xdotoolgui.threadmouse,(self,))
         #print 'sub thread id2222 : ', thread.get_ident()
 
     def aboutdialog1_activate(self, widget):
-        #self.wtree = gtk.glade.XML(sys.path[0]+'/xdotoolgui.glade', 'aboutdialog1')
-        self.aboutdialog1 = self.wtree.get_widget('aboutdialog1')
+        #self.builder = gtk.glade.XML(sys.path[0]+'/xdotoolgui.glade', 'aboutdialog1')
+        self.aboutdialog1 = self.builder.get_object('aboutdialog1')
         self.aboutdialog1.show()
         
     def quitabout(self, widget):
         self.aboutdialog1.hide()
         
     def newtext_activate(self, widget):
-        buffer=self.wtree.get_widget('textview1').get_buffer()
+        buffer=self.builder.get_object('textview1').get_buffer()
         buffer.set_text('')
         global filename
         filename = 'new file'
-        self.wtree.get_widget('statusbar1').push(self.wtree.get_widget('statusbar1').get_context_id("file name"),filename)
+        self.builder.get_object('statusbar1').push(self.builder.get_object('statusbar1').get_context_id("file name"),filename)
         
     def open_activate(self, widget):
-        #self.wtree = gtk.glade.XML(sys.path[0]+'/xdotoolgui.glade', 'filechooserdialog1')
-        self.filechooserdialog1 = self.wtree.get_widget('filechooserdialog1')
+        #self.builder = gtk.glade.XML(sys.path[0]+'/xdotoolgui.glade', 'filechooserdialog1')
+        self.filechooserdialog1 = self.builder.get_object('filechooserdialog1')
         self.filechooserdialog1.show()
         
     def filechoose_activate(self, widget):
         self.filechooserdialog1.hide()
-        #self.wtree.get_widget('window1').set_title('hehe')
-        buffer=self.wtree.get_widget('textview1').get_buffer()
+        #self.builder.get_object('window1').set_title('hehe')
+        buffer=self.builder.get_object('textview1').get_buffer()
         global filename
         filename=self.filechooserdialog1.get_filename()
-        self.wtree.get_widget('statusbar1').push(self.wtree.get_widget('statusbar1').get_context_id("FixStatus"),filename)
+        self.builder.get_object('statusbar1').push(self.builder.get_object('statusbar1').get_context_id("FixStatus"),filename)
         f = open(filename, "r")
         buf = f.read()
         f.close()
         buffer.set_text(buf)
     
     def save_activate(self, widget):
-        own = self.wtree.get_widget('textview1').get_buffer()
+        own = self.builder.get_object('textview1').get_buffer()
         start, end = own.get_bounds()
         chars = own.get_slice(start, end, False)
         global filename
-        self.wtree.get_widget('statusbar1').push(self.wtree.get_widget('statusbar1').get_context_id("FixStatus"),filename)
+        self.builder.get_object('statusbar1').push(self.builder.get_object('statusbar1').get_context_id("FixStatus"),filename)
         file = open(filename, "w")
         file.write(chars)
         file.close()
     def saveother_activate(self, widget):
-        self.filechooserdialog2 = self.wtree.get_widget('filechooserdialog2')
+        self.filechooserdialog2 = self.builder.get_object('filechooserdialog2')
         self.filechooserdialog2.show()
         
     def filesaveothercancel(self,widget):
         self.filechooserdialog2.hide()
     def filesaveother(self, widget):
         self.filechooserdialog2.hide()
-        buffer=self.wtree.get_widget('textview1').get_buffer()
+        buffer=self.builder.get_object('textview1').get_buffer()
         global filename
         filename = self.filechooserdialog2.get_filename()
-        own = self.wtree.get_widget('textview1').get_buffer()
+        own = self.builder.get_object('textview1').get_buffer()
         start, end = own.get_bounds()
         chars = own.get_slice(start, end, False)
-        self.wtree.get_widget('statusbar1').push(self.wtree.get_widget('statusbar1').get_context_id("FixStatus"),filename)
+        self.builder.get_object('statusbar1').push(self.builder.get_object('statusbar1').get_context_id("FixStatus"),filename)
         file = open(filename, "w")
         file.write(chars)
         file.close()
             
     def move_mouse(self, widget):
-        self.dialog1 = self.wtree.get_widget('dialog1')
+        self.dialog1 = self.builder.get_object('dialog1')
         self.dialog1.show()
-        self.wtree.get_widget('entry_x').set_text('0')
-        self.wtree.get_widget('entry_y').set_text('0')
+        self.builder.get_object('entry_x').set_text('0')
+        self.builder.get_object('entry_y').set_text('0')
     def movemousecancel(self, widget):
         self.dialog1.hide()
         
     def movethemouse(self, widget):
-        own = self.wtree.get_widget('textview1').get_buffer()
+        own = self.builder.get_object('textview1').get_buffer()
         start, end = own.get_bounds()
         chars = own.get_slice(start, end, False)
-        x=self.wtree.get_widget('entry_x').get_text()
-        y=self.wtree.get_widget('entry_y').get_text()
+        x=self.builder.get_object('entry_x').get_text()
+        y=self.builder.get_object('entry_y').get_text()
         chars = chars +"move mouse to "+x+" "+y+ "\n"
         own.set_text(chars)
         self.dialog1.hide()
     def delay_cancel(self, widget):
         self.dialog2.hide()
     def delay_time(self, widget):
-        self.dialog2 = self.wtree.get_widget('dialog2')
+        self.dialog2 = self.builder.get_object('dialog2')
         self.dialog2.show()
-        self.wtree.get_widget('entry_time1').set_text('1.0')
+        self.builder.get_object('entry_time1').set_text('1.0')
     def delay_clicked(self,widget):
-        own = self.wtree.get_widget('textview1').get_buffer()
+        own = self.builder.get_object('textview1').get_buffer()
         start, end = own.get_bounds()
         chars = own.get_slice(start, end, False)
-        x=self.wtree.get_widget('entry_time1').get_text()
+        x=self.builder.get_object('entry_time1').get_text()
         chars = chars +"delay "+x+" seconds\n"
         own.set_text(chars)
         self.dialog2.hide()
     def click_mouse(self,widget):
-        self.dialog3 = self.wtree.get_widget('dialog3')
+        self.dialog3 = self.builder.get_object('dialog3')
         self.dialog3.show()
-        self.wtree.get_widget('entry_sum3').set_text('1')
-        self.wtree.get_widget('entry_time3').set_text('0.0')
-        self.wtree.get_widget('combobox1').set_active(0)
+        self.builder.get_object('entry_sum3').set_text('1')
+        self.builder.get_object('entry_time3').set_text('0.0')
+        self.builder.get_object('combobox1').set_active(0)
     def mouseclick(self,widget):
-        own = self.wtree.get_widget('textview1').get_buffer()
+        own = self.builder.get_object('textview1').get_buffer()
         start, end = own.get_bounds()
         chars = own.get_slice(start, end, False)
-        x=self.wtree.get_widget('entry_sum3').get_text()
-        y=self.wtree.get_widget('entry_time3').get_text()
-        n = self.wtree.get_widget('combobox1').get_active()
+        x=self.builder.get_object('entry_sum3').get_text()
+        y=self.builder.get_object('entry_time3').get_text()
+        n = self.builder.get_object('combobox1').get_active()
         if n == 0:
             m = "left"
         if n == 1:
@@ -193,43 +217,43 @@ class xdotoolgui:
     def mouseclickcancel(self,widget):
         self.dialog3.hide()
     def keybord_clicked(self,widget):
-        self.dialog4 = self.wtree.get_widget('dialog4')
+        self.dialog4 = self.builder.get_object('dialog4')
         self.dialog4.show()
-        #self.wtree.get_widget('entry1').set_text(' ')
-        self.wtree.get_widget('entry_sum4').set_text('1')
-        self.wtree.get_widget('entry_time4').set_text('0.0')
+        #self.builder.get_object('entry1').set_text(' ')
+        self.builder.get_object('entry_sum4').set_text('1')
+        self.builder.get_object('entry_time4').set_text('0.0')
     def keybord_click(self,widget):
-        own = self.wtree.get_widget('textview1').get_buffer()
+        own = self.builder.get_object('textview1').get_buffer()
         start, end = own.get_bounds()
         chars = own.get_slice(start, end, False)
-        x=self.wtree.get_widget('entry_sum4').get_text()
-        y=self.wtree.get_widget('entry_time4').get_text()
-        key=self.wtree.get_widget('entry1').get_text()
+        x=self.builder.get_object('entry_sum4').get_text()
+        y=self.builder.get_object('entry_time4').get_text()
+        key=self.builder.get_object('entry1').get_text()
         chars = chars +"type keybord as "+key+" for "+x+" times then delay "+y+" seconds\n"
         own.set_text(chars)
         self.dialog4.hide()
     def keybordcancel(self,widget):
         self.dialog4.hide()
     def run(self,widget):
-        own = self.wtree.get_widget('textview1').get_buffer()
+        own = self.builder.get_object('textview1').get_buffer()
         start, end = own.get_bounds()
         chars = own.get_slice(start, end, False)
         global filename
-        self.wtree.get_widget('statusbar1').push(self.wtree.get_widget('statusbar1').get_context_id("FixStatus"),filename)
+        self.builder.get_object('statusbar1').push(self.builder.get_object('statusbar1').get_context_id("FixStatus"),filename)
         file = open(filename, "w")
         file.write(chars)
         file.close()
-        runxdotool.AddCommandToRun(filename)
+        self.add_command_to_run(filename)
     def mouselocation(self,widget):
         global MouseLocation
         a = 'get mouse location mode off'
         b = 'get mouse location mode on,now you can type any key like \'s\' to get the mouse location'
         if MouseLocation == True:
             MouseLocation = False
-            self.wtree.get_widget('statusbar1').push(self.wtree.get_widget('statusbar1').get_context_id("FixStatus"),a)
+            self.builder.get_object('statusbar1').push(self.builder.get_object('statusbar1').get_context_id("FixStatus"),a)
         else:
             MouseLocation = True
-            self.wtree.get_widget('statusbar1').push(self.wtree.get_widget('statusbar1').get_context_id("FixStatus"),b)
+            self.builder.get_object('statusbar1').push(self.builder.get_object('statusbar1').get_context_id("FixStatus"),b)
         #print 'main thread id : ', thread.get_ident()
         #thread.start_new_thread(xdotoolgui.threadmouse,(self,))
         #print 'main thread id2 : ', thread.get_ident()
@@ -237,22 +261,52 @@ class xdotoolgui:
         #print 'sub thread id : ', thread.get_ident()
         global MouseLocation
         if MouseLocation:
-            glib.timeout_add(100, self.ThreadMouse)
+            GLib.timeout_add(100, self.ThreadMouse)
     def ThreadMouse(self):
-        f = gtk.Window()
+        f = Gtk.Window()
         rootwin = f.get_screen().get_root_window()
-        x,y,mods = rootwin.get_pointer()
+        pointer = rootwin.get_pointer()
+        x,y = pointer.x,pointer.y
         s = 'mouse location is ('+str(x)+',' + str(y)+')'
-        self.wtree.get_widget('statusbar1').push(self.wtree.get_widget('statusbar1').get_context_id("FixStatus"),s)
-        own = self.wtree.get_widget('textview1').get_buffer()
+        self.builder.get_object('statusbar1').push(self.builder.get_object('statusbar1').get_context_id("FixStatus"),s)
+        own = self.builder.get_object('textview1').get_buffer()
         start, end = own.get_bounds()
         chars = own.get_slice(start, end, False)
         chars = chars +"move mouse to "+str(x)+" "+str(y)+ "\n"
         own.set_text(chars)
+    def add_command_to_run(self, command_path):
+        # print(command_path)
+        self.command_path = command_path
+        afile = open(self.command_path)
+        command_parameters = afile.read().split("\n")
+        for s in command_parameters:
+            command = s.split(" ")
+            if command[0] == 'move':
+                subprocess.check_call(["xdotool","mousemove",command[3],command[4]])
+            if command[0] == 'delay':
+                time.sleep(string.atof(command[1]))
+            if command[0] == 'click':
+                if command[2] == 'left':
+                    for i in command[5]:
+                        subprocess.check_call(["xdotool","click","1"])
+                        time.sleep(string.atof(command[9]))
+                if command[2] == 'middle':
+                    for i in command[5]:
+                        subprocess.check_call(["xdotool","click","2"])
+                        time.sleep(string.atof(command[9]))
+                if command[2] == 'right':
+                    for i in command[5]:
+                        subprocess.check_call(["xdotool","click","3"])
+                        time.sleep(string.atof(command[9]))
+            if command[0] == 'type':
+                for i in command[5]:
+                    subprocess.check_call(["xdotool","key",command[3]])
+                    time.sleep(string.atof(command[9]))
+        return 0
             #time.sleep(0.01)
         #print 'sub thread id2 : ', thread.get_ident()
 #thread.start_new_thread(xdotoolgui.threadmouse,())
 if __name__ == "__main__":
     editor =xdotoolgui()
     #editor.window.show()
-    gtk.main()
+    Gtk.main()
